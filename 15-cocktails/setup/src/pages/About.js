ee7900378d540ecd4a67db4fa862eb0c13237f3c
Piotr Drajewicz <drpiterek@gmail.com>
2022-11-25 22:12:33 +0100

    added SharedNavbar

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque,
+        asperiores. Adipisci hic omnis ut officiis temporibus dicta libero,
+        consectetur perspiciatis laborum fuga, nemo odio? Atque dolores, beatae
+        nesciunt excepturi possimus neque ratione unde, maxime similique commodi
+        quidem facere ad modi.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
-}
+    <section className="section error-page">
+      <div className="error-container">
+        <h1>Page not found</h1>
+        <Link to="/" className="btn btn-primary">
+          Back home
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default Error
+export default Error;
